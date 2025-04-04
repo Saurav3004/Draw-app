cf083a46283b94fa32860dc44273bf46005ceff7
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-04-05 04:13:19 +0530

    http-endpoints connect to database

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,32 +2,35 @@ import express from 'express'
 import jwt from "jsonwebtoken"
 import { middleware } from './middleware'
 import { JWT_SECRET_KEY } from '@repo/backend-common/config'
-import {CreateUserSchema} from '@repo/common/types'
+import {CreateRoomSchema, CreateUserSchema, SignInSchema} from '@repo/common/types'
 import { prismaClient } from '@repo/db/client'
+import bcryptjs from 'bcryptjs'
 
 const app = express()
-
+app.use(express.json())
 
 
 app.post("/signup",async (req,res) => {
     const parsedData = CreateUserSchema.safeParse(req.body);
+    
     if(!parsedData.success){
         res.json({
             message: "Incorrect inputs"
         })
         return;
     }
-
+ 
   try {
-    await prismaClient.user.create({
+    const hashedPassword = await bcryptjs.hash(parsedData.data.password,10);
+   const user = await prismaClient.user.create({
       data:{
           email: parsedData.data.email,
           name: parsedData.data.name,
-          password: parsedData.data.password
+          password: hashedPassword
       }
      })
       res.json({
-          userdId: "123"
+          userdId: user
       })
   } catch (error) {
     res.status(411).json({
@@ -37,21 +40,83 @@ app.post("/signup",async (req,res) => {
     
 })
 
-app.post("/signin",(req,res) => {
-    const userId = 1;
-    const token = jwt.sign({userId},JWT_SECRET_KEY)
+app.get("/signin",async (req,res) => {
+    try {
+        const parsedData = SignInSchema.safeParse(req.body);
+        if(!parsedData.data){
+            res.status(411).json({
+                message: "All fields required"
+            })
+            return;
+        }
+        
+        const userExists = await prismaClient.user.findFirst({
+            where:{
+                email: parsedData.data.email 
+            }
+        })
+        if(!userExists){
+            res.status(411).json({
+                message: "No user exists with this email"
+            })
+            return;
+        }
+        // @ts-ignore 
+        const verifyPassword = await bcryptjs.compare(parsedData.data.password,userExists.password)
+        if(verifyPassword){
+            const token = jwt.sign(userExists.id,JWT_SECRET_KEY)
+            res.status(200).json({
+                message: "Login successfully",
+                token
+            })
 
-    res.json({
-        token
-    })
+        }else{
+            res.status(411).json({
+                message: "Wrong password or email"
+            })
+            return ;
+        }
+        
+    
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.post("/create-room",middleware,(req,res) => {
-    //db-call
+app.post("/room",middleware,async (req,res) => {
+    
+        const parsedData = CreateRoomSchema.safeParse(req.body);
+        if(!parsedData.data){
+            res.status(411).json({
+                message: "Room name is required"
+            })
+            return;
+        }
+        
+        try {
+            // @ts-ignore 
+        const userId = req.userId
+        
+            const room = await prismaClient.room.create({
+                data:{
+                    slug: parsedData.data.name,
+                    adminId: userId
+                }
+            })
+    
+            res.status(200).json({
+                room
+            })
+        
+        }
+     catch (error) {
+       res.status(500).json({
+        message: "Room already exists with this name.try another name"
+       })
+    }
 
-    res.json({
-        roomId:123
-    })
+   
 })
 
 app.listen(5000)
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -3,15 +3,20 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET_KEY } from "@repo/backend-common/config";
 
 export function middleware(req:Request,res:Response,next:NextFunction) {
-    const token = req.headers["authorization"] ?? "";
-    const decoded = jwt.verify(token,JWT_SECRET_KEY)
+    try {
+        const token = req.headers["authorization"] ?? "";
+        const decoded = jwt.verify(token,JWT_SECRET_KEY)
+        if(decoded){
+            //@ts-ignore
+            req.userId = decoded
+            
+            next()
+        }
+    } catch (error) {
     
-    if(!decoded){
-        res.status(403).json({
-            message: "you are not authenticated"
-        })
-    }else{
-        //@ts-ignore
-        req.userId = decoded.userId
+            res.status(411).json({
+                message: "Unauthorized"
+            })
+        
     }
 }
