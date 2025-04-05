0e7158f02c273efc5595f37858e1d829ca74a3bc
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-04-05 12:35:08 +0530

    ws layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,32 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET_KEY } from "@repo/backend-common/config";
 
 
 const wss = new WebSocketServer({port:8080});
+
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
+}
+
+const users:User[] = []
+
+function checkUser(token:string):string | null{
+    const decoded = jwt.verify(token,JWT_SECRET_KEY)
+
+    if(typeof decoded == "string"){
+       
+        return null;
+    }
+
+    if(!decoded || !decoded.userId){
+        
+        return null;
+    }
+    return decoded.userId;
+}
 
 wss.on("connection",(ws,request) => {
     const url = request.url;
@@ -14,16 +37,24 @@ wss.on("connection",(ws,request) => {
     const getParams = new URLSearchParams(url.split("?")[1]);
     const token = getParams.get('token') || "";
 
-    const decoded = jwt.verify(token,JWT_SECRET_KEY)
-
-    if(typeof decoded == "string"){
-        ws.close()
-        return;
-    }
-
-    if(!decoded || !decoded.userId){
+    const userId = checkUser(token);
+    if(userId == null){
         ws.close();
-        return;
+        return null;
     }
+
+    users.push({
+        userId,
+        rooms:[],
+        ws
+    })
+
+    ws.on("message",function message(data){
+        const parsedData = JSON.parse(data as unknown as string)
+        if(parsedData.type === "join_room"){
+            const user = users.find(x => x.ws === ws);
+            user?.rooms.push(parsedData.roomId)
+        }
+    })
 
 })
